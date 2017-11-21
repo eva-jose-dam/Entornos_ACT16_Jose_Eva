0a93c3e7bc5d78b04af5f5b17912a0cda31d6516
Eva y Jose <eva-jose-dam@protonmail.com>
2017-11-21 22:42:16 +0100

    Jose edita la clase principal.

--- src/clases_conjuntas/EvaJose.java
+++ src/clases_conjuntas/EvaJose.java
@@ -6,7 +6,13 @@ public class EvaJose {
 		System.out.println("Este es el texto por defecto de este proyecto. "
 				+ "Tanto Eva como Jose han editado esta clase y su texto para "
 				+ " verificar que saben implementar cambios tanto en local como remoto"
-				+ "son unos cracks!");
+				+ "son unos cracks!"
+				+ "Soy Jose y para que no haya dudas voy a contar un chiste muy malo: "
+				+ "Se abre el telón, aparece un programador y dice: ¿¡QUÉ HABÉIS TOCADO QUE NO SE CIERRA EL TELÓN!?"
+				+ "..."
+				+ "..."
+				+ "Eva, creo que acabamos de suspender Entornos. Un abrazo.");
+		
 	}
 
 }
