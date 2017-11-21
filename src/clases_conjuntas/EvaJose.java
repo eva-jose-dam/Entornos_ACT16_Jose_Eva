e248a05f80bfa076f29ba8dabc11171a44194e56
Eva <eva@prueba.com>
2017-11-21 15:24:59 +0100

    Version 1. Se sube el proyecto a GitHub

---
+++ src/clases_conjuntas/EvaJose.java
@@ -0,0 +1,12 @@
+package clases_conjuntas;
+
+public class EvaJose {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el texto por defecto de este proyecto. "
+				+ "Tanto Eva como Jose han editado esta clase y su texto para "
+				+ " verificar que saben implementar cambios tanto en local como remoto"
+				+ "son unos cracks!");
+	}
+
+}
